e354fcaeb27d4364e501a8e1db4b9310360de8ce
aryan-garg0729 <155893692+aryan-garg0729@users.noreply.github.com>
2024-10-20 11:54:59 +0530

    enhanced performance

--- String Hashing.cpp
+++ String Hashing.cpp
@@ -1,19 +1,19 @@
 class Hash{
-    vector<pair<long long, long long>> prefixHash; 
-    vector<pair<long long, long long>> powers; 
+    vector<pair<int, int>> prefixHash; 
+    vector<pair<int, int>> powers; 
     const int BASE1; 
     const int BASE2;
     int mod; 
     public:
-    Hash(string str):BASE1(4691),BASE2(7591){
+    Hash(string&str):BASE1(4691),BASE2(7591){
         mod = 1e9 + 7;
         prefixHash.resize(str.size() + 1, {0, 0});
         powers.resize(str.size() + 1, {1, 1});
         // precompute powers 
         for (int i = 1; i < str.size(); i++){
             powers[i] = {
-                (powers[i - 1].first * BASE1)%mod, 
-                (powers[i - 1].second * BASE2)%mod
+                (1ll*powers[i - 1].first * BASE1)%mod, 
+                (1ll*powers[i - 1].second * BASE2)%mod
             };
         }
         // compute prefixhash
@@ -29,8 +29,8 @@ class Hash{
     pair<int, int> getHash(int left, int right){
         left++; 
         right++; 
-        return {(prefixHash[right].first - (prefixHash[left - 1].first * powers[right - left + 1].first) % mod + mod) % mod, 
-                (prefixHash[right].second - (prefixHash[left - 1].second * powers[right - left + 1].second) % mod + mod) % mod};
+        return {(prefixHash[right].first - (1ll*prefixHash[left - 1].first * powers[right - left + 1].first) % mod + mod) % mod, 
+                (prefixHash[right].second - (1ll*prefixHash[left - 1].second * powers[right - left + 1].second) % mod + mod) % mod};
     }
 
 };
